eb8549336be781815848661e6eec5685293f1a44
marcram2006 <marcram.a.uni@gmail.com>
2025-04-27 19:07:21 +0200

    Update fitxa.h

--- fitxa.h
+++ fitxa.h
@@ -31,14 +31,24 @@ public:
 	void afegirMoviment();
 	void netejaMoviments();
 
-	void movimentsValids(const Tauler& t);
 	void convertirDama();
+	int getX() { m_posicio.getX(); }
+	int getY() { m_posicio.getY(); }
 	ColorFitxa getColor() const { return m_color; }
 	TipusFitxa getTipus() const { return m_tipus; }
 	Posicio getPosicio() const { return m_posicio; }
 
-	bool operator==(const Fitxa& f);
+	void setColor(ColorFitxa c) { m_color = c; }
+	void setTipus(TipusFitxa t) { m_tipus = t; }
 
+	void setPosicioBuida(const Posicio& p);
+
+	int getTipusFitxa();
+
+	bool operator==(const Fitxa& f);
+	void setPosNova(const Posicio& pos, ColorFitxa c, TipusFitxa t);
+
+	void posicionsMoviment();
 
 private:
 	Posicio m_posicio;
