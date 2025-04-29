b577ccfd118c0c0ff6bf11487ee67a6f1dfe919b
marcram2006 <marcram.a.uni@gmail.com>
2025-04-29 22:31:05 +0200

    Update fitxa.cpp

--- fitxa.cpp
+++ fitxa.cpp
@@ -88,7 +88,7 @@ void Fitxa::calcularMovimentsValids2(
         Moviment movimentActual = movimentsPendents[inici++];
         Posicio  posicioActual = movimentActual.ultimPosicio(m_posicio);
 
-        // Calculem les posicions valides des d’aquesta posicio
+        // Calculem les posicions valides des dâ€™aquesta posicio
         Posicio posValides[8];
         int     nPosValides = 0;
 
@@ -115,7 +115,7 @@ void Fitxa::calcularMovimentsValids2(
                     movimentsPendents[final++] = nouMov;
             }
 
-            // Continuem el bucle des de la nova posició
+            // Continuem el bucle des de la nova posiciÃ³
             posicioActual = posValides[0];
             tauler.getPosicionsValides(
                 posicioActual,
@@ -125,11 +125,92 @@ void Fitxa::calcularMovimentsValids2(
                 posValides);
         }
 
-        // Si el moviment final no és buit, el guardem
+        // Si el moviment final no Ã©s buit, el guardem
         if (movimentActual.getNombre() > 0
             && nValid < MAX_MOVIMENTS)
         {
             movimentsValids[nValid++] = movimentActual;
         }
+    }
+}
+
+
+string Fitxa::posicionsMoviment()
+{
+	string s;
+	for (int i = 0; i < m_nPosicionsValides - 2; i++)
+	{
+		s += m_moviments.getPosicioIndex(i).toString();
+		s += " ,";
+	}
+	s += m_moviments.getPosicioIndex(m_nPosicionsValides - 1).toString();
+	return s;
+}
+
+Posicio Fitxa::movimentsValids(int n)
+{
+	m_movimentsPossibles.getPosicioIndex(n);
+}
+
+
+
+void Fitxa::movimentNormal(const Tauler& tauler)
+{
+    Posicions mValids[100];
+    int nValids = 0
+    Posicions mPendents[100];
+    int nPendents = 0;
+
+    int nPosicions = 0;
+    int x = m_posicio.getX();
+    int y = m_posicio.getY();
+    if (m_color == COLOR_BLANC)
+    {
+        if (tauler.getTipusFitxa(x + 1, y + 1) == TIPUS_EMPTY)
+        {
+            Posicio p(x + 1, y + 1);
+            m_moviments[nPosicions].afegirPosicio(p);
+        }
+        else
+        {
+            if (tauler[x + 1][y + 1].getColor() == COLOR_NEGRE && tauler[x + 2][y + 2].getTipusFitxa() == TIPUS_EMPTY)
+            {
+                mValids[nPendents++] = Posicio(x + 2, x + 2);
+                mPendents[nPendents++] = Posicio(x + 2, x + 2);
+                Posicio p;
+                do
+                {
+                    p = mPendents[0];
+                    x = p.getX();
+                    y = p.getY();
+                    for (int i = 0; i > nPendents - 1; i++)
+                    {
+                        mPendents[i] = mPendents[i + 1];
+                    }
+                    nPentdents--;
+                    if (tauler[x + 1][y + 1].getColor() == COLOR_NEGRE && tauler[x + 2][y + 2].getTipusFitxa() == TIPUS_EMPTY)
+                    {
+                        if (//esta dentro de alguno q no lo repita)
+                        mValids[nValids++] = Posicio(x + 2, x + 2);
+                        mPendents[nPendents++] = Posicio(x + 2, x + 2);
+                    }
+                    if (tauler[x - 1][y + 1].getColor() == COLOR_NEGRE && tauler[x - 2][y + 2].getTipusFitxa() == TIPUS_EMPTY)
+                    {
+                        mValids[nValids++] = Posicio(x + 2, x - 2);
+                        mPendents[nPendents++] = Posicio(x + 2, x - 2);
+                    }
+                    mPendents[nPendents++] = Posicio(x+2, x+2)
+                }while (mPendents != []);
+            }
+        }
+        if (getTipusFitxa(x - 1, y + 1) == TIPUS_EMPTY)
+        {
+            Posicio p(x - 1, y + 1);
+            m_moviments[nPosicions].afegirPosicio(p);
+        }
+    }
+    else
+    {
+
     }
 }
