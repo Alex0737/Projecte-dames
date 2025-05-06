20b59246e559bb69d6c9250157a5b2aa6de56831
marcram2006 <marcram.a.uni@gmail.com>
2025-05-06 23:59:27 +0200

    Update tauler.cpp

--- tauler.cpp
+++ tauler.cpp
@@ -258,128 +258,6 @@ bool Tauler::dinsTauler(int x, int y) const
 	return ((x < N_FILES) && (y < N_COLUMNES) && x > 0 && y > 0);
 }
 
-void Tauler::calcularMovimentsValids(const Fitxa& fitxa) const
-{
-	int x = fitxa.getX();
-	int y = fitxa.getY();
-	bool acabat = false;
-	Fitxa f = m_tauler[x][y];
-	Moviments pendents[MAX_MOVS_FITXA];
-	int inici = 0, final = 0;
-	pendents[final++] = Moviments();
-
-	int direccions[4][2];
-	int nDireccions = 0;
-
-	if (f.getTipus() == TIPUS_NORMAL)
-	{
-		nDireccions = 2;
-		if (f.getColor() == COLOR_BLANC)
-		{
-			direccions[0][0] = -1;
-			direccions[0][1] = -1;
-			direccions[1][0] = -1;
-			direccions[1][1] = 1;
-		}
-		else
-		{
-			direccions[0][0] = 1;
-			direccions[0][1] = -1;
-			direccions[1][0] = 1;
-			direccions[1][1] = 1;
-		}
-	}
-	else
-	{
-		if (f.getTipus() == TIPUS_DAMA)
-		{
-			nDireccions = 4;
-			direccions[0][0] = 1;
-			direccions[0][1] = 1;
-			direccions[1][0] = 1;
-			direccions[1][1] = -1;
-			direccions[2][0] = -1;
-			direccions[2][1] = 1;
-			direccions[3][0] = -1;
-			direccions[3][1] = -1;
-		}
-	}
-
-
-	while (inici < final)
-	{
-		Moviments actual = pendents[inici++];
-		Posicio  posActual = actual.getUltimaPosicio();
-		int contadorDireccions = 0;
-		while (contadorDireccions < nDireccions)
-		{
-			int dx = direccions[contadorDireccions][0];
-			int dy = direccions[contadorDireccions][1];
-			int x = posActual.getX();
-			int y = posActual.getY();
-			bool trobat = false;
-			bool menjada = false;
-
-			Moviments nouMov = actual;
-
-			int iteracions = 0;
-			int maxIter;
-			if (f.getTipus() == TIPUS_DAMA)
-			{
-				maxIter = 7;
-			}
-			else
-			{
-				maxIter = 1;
-			}
-
-			while (iteracions < maxIter && !acabat)
-			{
-				x += dx;
-				y += dy;
-				iteracions++;
-
-				if (dinsTauler(x, y))
-				{
-					Fitxa casella = getFitxa(x, y);
-
-					if (casella.getTipus() == TIPUS_EMPTY && !menjada)
-					{
-						if (actual.getMenjades() == 0)
-						{
-							nouMov.afegirPosicio(Posicio(x, y));
-							f.afegirMoviment(nouMov);
-						}
-					}
-					else
-					{
-						if (casella.getColor() != f.getColor())
-						{
-							int xSalt = x + dx;
-							int ySalt = y + dy;
-							if (dinsTauler(xSalt, ySalt) && getFitxa(xSalt, ySalt).getTipus() == TIPUS_EMPTY)
-							{
-								nouMov.afegirPosicio(Posicio(xSalt, ySalt));
-								nouMov.incrementarMenjades();
-								if (getTipusFitxa(x, y) == TIPUS_DAMA)
-								{
-									nouMov.incrementarMenjadesDames();
-								}
-								nouMov.setMenjat();
-								pendents[final++] = nouMov;
-								trobat = true;
-
-							}
-						}
-					}
-
-				}
-
-			}
-			contadorDireccions++;
-		}
-	}
-}
 
 
 
@@ -566,3 +444,128 @@ void Tauler::calcularMovimentsValids(const Fitxa& fitxa) const
 	}
 }
 
+
+
+
+void Tauler::calcularMovimentsValids(const Fitxa& fitxa) const
+{
+	int x = fitxa.getX();
+	int y = fitxa.getY();
+	bool acabat = false;
+	Fitxa f = m_tauler[x][y];
+	Moviments pendents[MAX_MOVS_FITXA];
+	int inici = 0, final = 0;
+	pendents[final++] = Moviments();
+
+	int direccions[4][2];
+	int nDireccions = 0;
+
+	if (f.getTipus() == TIPUS_NORMAL)
+	{
+		nDireccions = 2;
+		if (f.getColor() == COLOR_BLANC)
+		{
+			direccions[0][0] = -1;
+			direccions[0][1] = -1;
+			direccions[1][0] = -1;
+			direccions[1][1] = 1;
+		}
+		else
+		{
+			direccions[0][0] = 1;
+			direccions[0][1] = -1;
+			direccions[1][0] = 1;
+			direccions[1][1] = 1;
+		}
+	}
+	else
+	{
+		if (f.getTipus() == TIPUS_DAMA)
+		{
+			nDireccions = 4;
+			direccions[0][0] = 1;
+			direccions[0][1] = 1;
+			direccions[1][0] = 1;
+			direccions[1][1] = -1;
+			direccions[2][0] = -1;
+			direccions[2][1] = 1;
+			direccions[3][0] = -1;
+			direccions[3][1] = -1;
+		}
+	}
+
+
+	while (inici < final)
+	{
+		Moviments actual = pendents[inici++];
+		Posicio  posActual = actual.getUltimaPosicio();
+		int contadorDireccions = 0;
+		while (contadorDireccions < nDireccions)
+		{
+			int dx = direccions[contadorDireccions][0];
+			int dy = direccions[contadorDireccions][1];
+			int x = posActual.getX();
+			int y = posActual.getY();
+			bool trobat = false;
+			bool menjada = false;
+
+			Moviments nouMov = actual;
+
+			int iteracions = 0;
+			int maxIter;
+			if (f.getTipus() == TIPUS_DAMA)
+			{
+				maxIter = 7;
+			}
+			else
+			{
+				maxIter = 1;
+			}
+
+			while (iteracions < maxIter && !acabat)
+			{
+				x += dx;
+				y += dy;
+				iteracions++;
+
+				if (dinsTauler(x, y))
+				{
+					Fitxa casella = getFitxa(x, y);
+
+					if (casella.getTipus() == TIPUS_EMPTY && !menjada)
+					{
+						if (actual.getMenjades() == 0)
+						{
+							nouMov.afegirPosicio(Posicio(x, y));
+							f.afegirMoviment(nouMov);
+						}
+					}
+					else
+					{
+						if (casella.getColor() != f.getColor())
+						{
+							int xSalt = x + dx;
+							int ySalt = y + dy;
+							if (dinsTauler(xSalt, ySalt) && getFitxa(xSalt, ySalt).getTipus() == TIPUS_EMPTY)
+							{
+								nouMov.afegirPosicio(Posicio(xSalt, ySalt));
+								nouMov.incrementarMenjades();
+								if (getTipusFitxa(x, y) == TIPUS_DAMA)
+								{
+									nouMov.incrementarMenjadesDames();
+								}
+								nouMov.setMenjat();
+								pendents[final++] = nouMov;
+								trobat = true;
+
+							}
+						}
+					}
+
+				}
+
+			}
+			contadorDireccions++;
+		}
+	}
+}
